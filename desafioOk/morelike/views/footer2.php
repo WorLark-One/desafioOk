2faf0820e1400913f6446bf74af86857bd86fc0c
felipe <felipe.milla.calquin@gmail.com>
2020-10-22 17:02:59 -0300

    inicio
    this

---
+++ desafioOk/morelike/views/footer2.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+  <footer class="footer" style="margin: 30px">
+    <div class="container" style="text-align:center; font-size:12px;">
+        <p class="muted credit">
+        Developed by <a href="#">Felipe Andres Milla Calquin - Brayan Escobar Garrido</a></p>
+      </div>
+  </footer>
+</body>
+
+</html>
