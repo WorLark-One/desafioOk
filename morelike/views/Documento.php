6e2aa6da492af0ae310106cef51564dcd10046cd
felipe <felipe.milla.calquin@gmail.com>
2020-11-04 21:32:52 -0300

    arreglo visual documento.php
    this

--- morelike/views/Documento.php
+++ morelike/views/Documento.php
@@ -1,45 +1,39 @@
+
 <div class="row">
-	<div class="col-sm">
-		<table class="table table-striped">
+	<div class="col-12">
+		<div class="col-12 mt-4">
+			<h3 class="text-center">Ultimo acceso usuarios</h3>
+		</div>
+		<table class="table table-striped mt-4">
+			<th>Rut</th>
+			<th>Nombre</th>
+			<th>Rol</th>
+			<th>Ultimo acceso</th>
+			<?php foreach($users as $row):?>
+				<tr>
+				<td><text><?=$row->rut?></text></td>
+				<td><text><?=$row->nombre?></text></td>
+				<td><text><?=$row->rol?></text></td>
+				<td><text><?=$row->acceso?></text></td>
+				</tr> 
+			<?php endforeach;?>
+		</table>
+	</div>
+
+	<div class="col-12">
+		<div class="col-12 mt-4">
+			<h3 class="text-center">Montos final de ingresos/egresos por día</h3>
+		</div>
+		<table class="table table-striped mt-4">
 			<th>Ingresos</th>
 			<th>Egreso</th>
 			<!-- <th></th>
 			<th></th>
 			<th></th> -->
-	
 			<tr>
 			<td><text>$10.000</text></td>
 			<td><text>$20.000</text></td>
-			<tr>
-
-		</table>
-	</div>
-</div>
-<div class="row">
-	<div class="col">
-		<table class="table table-striped">
-			<th>Rut</th>
-			<th>Nombre</th>
-			<th>Rol</th>
-			<th>Especialidad</th>
-			<th>Ultimo acceso</th>
-			<th>Ingreso final</th>
-			<th>EgresoFinal</th>
-			<th></th>
-			<th></th>
-			<th></th>
-			<?php foreach($users as $row):?>
-				<?php if($row->rol !="Administrador"):?>  
-				
-					<tr>
-					<td><text><?=$row->rut?></text></td>
-					<td><text><?=$row->nombre?></text></td>
-					<td><text><?=$row->rol?></text></td>
-					<td><text><?=$row->especialidad?></text></td>
-					<td><text><?=$row->acceso?></text></td>
-					</td> 
-				<?php endif;?>
-			<?php endforeach;?>
+			</tr>
 		</table>
 	</div>
 </div>
